b03cee794c573676ca8da09be4f43695c9db9347
LucasMattera <lucasmattera97@gmail.com>
2021-05-06 17:02:05 -0300

    fix parseo de data

--- src/belongs-classes/trackBelongs.js
+++ src/belongs-classes/trackBelongs.js
@@ -2,15 +2,15 @@ const { Belongs } = require('./belongs')
 
 class TrackBelongs extends Belongs{
     execute(trackData){
-        const n_ame = trackData[0]
-        const duration = trackData[1]
-        const genres = trackData[2]
+        const n_ame = trackData.name;
+        const duration = trackData.duration;
+        const genres = trackData.genres;
 
-        let check_name = this.instances_of_domain.some(track => track.name === n_ame) 
-        let check_duration = this.instances_of_domain.some(track => track.duration === duration) 
-        let check_genres = this.instances_of_domain.some(track => track.genres === genres) 
+        let check_name = this.instances_of_domain.some(track => track.name === n_ame) ;
+        let check_duration = this.instances_of_domain.some(track => track.duration === duration) ;
+        let check_genres = this.instances_of_domain.some(track => track.genres === genres) ;
 
-        return check_name && check_duration && check_genres
+        return check_name && check_duration && check_genres;
 
     }
 }
