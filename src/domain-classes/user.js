aac7bfea3caa5649ac27465bd2af175051e5b58b
franarci <fran.arciniega96@gmail.com>
2021-05-20 19:00:35 -0300

    cambio listened en usuario para que picklify pueda persistirlo

--- src/domain-classes/user.js
+++ src/domain-classes/user.js
@@ -2,6 +2,8 @@ const { Album } = require("./album");
 const { Artist } = require("./artist");
 const track = require("./track");
 const { Track } = require("./track");
+var stringify = require('json-stringify-safe');
+const artist = require("./artist");
 
 class User {
     constructor(id, username){
@@ -11,33 +13,52 @@ class User {
     }
 
     listenTrack(track){
+
         //listened tracks es un map donde la clave es el nombre de un track y el valor es un par [Track, vecesEscuchada<Int>]
         if(!this.hasListened(track)){
-            this.listened.set(track,1);
+            this.listened[stringify(track)]=1;
         } else {
-            const tListened = this.timesListened(track);
-            this.listened.set(track, tListened+1);
+            this.listened[stringify(track)]=this.timesListened(track)+1 ;
         }
     }
     
     hasListened(track){
-        let tracks = Array.from(this.listened.keys());
+        let tracks = Object.keys(this.listened);
+        if(tracks.length!==0){
+            tracks = tracks.map( t => JSON.parse(t))
+        }
+        
         return tracks.some(t => 
+
                         this.sameTrackName(t.name, track.name)
-                        );
+                        )
     }
 
     sameTrackName(tn1,tn2){
         return(tn1.localeCompare(tn2) === 0);
     }
 
-    getListened(){  
-        return this.listened.keys();
+    getListened(){ 
+        let tracks = Object.keys(this.listened);
+        if(tracks.length!==0){// EN ESTE PUNTO tracks es una lista vacia o una lista con un string conteniendo todos los tracks
+            tracks = tracks.map( t => JSON.parse(t))
+            tracks = tracks.map( t =>
+                    new Track(
+                        t.id,
+                        t.name,
+                        t.duration,
+                        t.album,
+                        t.genres,
+                        t.artist
+                    )
+                )
+        } 
+        return tracks
     }
 
     timesListened(track){
         if(this.hasListened(track)){
-            return this.listened.get(track);
+            return this.listened.get(stringify(track));
         } else {
             return 0;
         }
@@ -46,8 +67,9 @@ class User {
     getTracks(artist){//Devuelve la lista de tracks del artista "artist" con sus respectivas timesListened 
         return Array.from(this.listened).filter(([track, n]) => track.artist.id === artist.id);
     }
-
 }
+
+
 
 module.exports = {
     User: User,
