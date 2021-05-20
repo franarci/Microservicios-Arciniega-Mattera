279cd60e8ac0ceb8cc8d237ab354e3bec832cfe7
LucasMattera <lucasmattera97@gmail.com>
2021-05-20 00:05:27 -0300

    add test delete track

--- test.js
+++ test.js
@@ -233,12 +233,28 @@ describe('Add, remove and filter data', () => {
 
     it('test nuestro - when delete a track should delete it from the artist', () => {
         const artist = createAndAddArtist(unqfy, 'Guns n\' Roses', 'USA');
-        const album = createAndAddAlbum(unqfy, artist.id, 'Appetite for Destruction', 1987);
-        const track = createAndAddTrack(unqfy, album.id, 'Welcome to the jungle', 200, ['rock', 'hard rock']);
 
-        unqfy.deleteTrack(track);
+        const album1 = createAndAddAlbum(unqfy, artist.id, 'Appetite for Destruction', 1987);
+        const track1 = createAndAddTrack(unqfy, album1.id, 'Welcome to the jungle', 200, ['rock', 'hard rock']);
+        const track2 = createAndAddTrack(unqfy, album1.id, 'Welcome to the jungle', 200, ['rock', 'hard rock']);
 
-        assert.isFalse(artist.tracks.includes(track));
+        const album2 = createAndAddAlbum(unqfy, artist.id, 'Appetite for Destruction', 1987);
+        const track3 = createAndAddTrack(unqfy, album2.id, 'Welcome to the jungle', 200, ['rock', 'hard rock']);
+        const track4 = createAndAddTrack(unqfy, album2.id, 'Welcome to the jungle', 200, ['rock', 'hard rock']);
+
+        const albums = artist.getAlbums();
+
+        unqfy.deleteTrack(track1);
+        unqfy.deleteTrack(track2);
+
+        unqfy.deleteTrack(track3);
+        unqfy.deleteTrack(track4);
+
+        assert.isFalse(albums[0].tracks.includes(track1));
+        assert.isFalse(albums[0].tracks.includes(track2));
+
+        assert.isFalse(albums[1].tracks.includes(track3));
+        assert.isFalse(albums[1].tracks.includes(track4));
     });
 
     it('test nuestro - when delete a track should delete a track from the playlists', () => { 
