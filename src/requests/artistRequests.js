14b1761c8f03b8b80552f2f6abcdbac2d94c603e
LucasMattera <lucasmattera97@gmail.com>
2021-06-04 17:39:39 -0300

    add directorio de requests, artistsRequests module

---
+++ src/requests/artistRequests.js
@@ -0,0 +1,13 @@
+const rp = require('request-promise');
+
+const options = {
+        url: 'https://api.spotify.com/v1/search',
+        headers: { Authorization: 'Bearer ' + 'BQDvkJi6yWTdi50e-yQ0ruNiI0ikkAftrTeAtme0OV2oxyYOmABMAjZbeo2ipAXwopqdFC6mq0QsKMTgX1DUEnuV2xL44WTcQgzrTXfKAFYMkbpgk-cWQ4pIvemhdzzX1p_5hEYkQncg5S9Dlj3ENJjLEm3b' },
+        json: true
+};
+
+// GET /api/artists?name=
+
+rp.get(options).then((response) => //hacer algo con response
+);
+
