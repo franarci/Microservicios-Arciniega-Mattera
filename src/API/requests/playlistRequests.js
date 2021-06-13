84911892e43d2dfa889a1d69efe2e8194f93846e
franarci <fran.arciniega96@gmail.com>
2021-06-13 20:45:25 -0300

    playlist requests

--- src/API/requests/playlistRequests.js
+++ src/API/requests/playlistRequests.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {getUNQfy, saveUNQfy} = require('../../../main');
 const newTknModule = require('../spotify/getSpotifyToken');
-const errors = require('../apiErrors');
+const { errorHandler } = require('../apiErrors');
 
 const unqfy = getUNQfy();
 const appPlaylist = express();
@@ -26,10 +26,54 @@ function standardJSONOutput(playlist){
         tracks: getNotRecursiveTracks(playlist.tracks),
     }
 }
-
+appPlaylist.use(errors.errorHandler);
 appPlaylist.use('/playlists', router);
 
-//router.use('/')  appendear las requests aca <<<-------
+router.use('/')
+    .post((req, res, err) => { // POST /api/playlist
+        try{
+            if(req.body.keys.length==3){
+                unqfy.createPlaylist(req.body.name,null,req.body.maxDuration,req.body.name.genres);
+                const playlist = unqfy.getInstanceByAttribute(req.body.name, 'playlist', 'name');
+                saveUNQfy(unqfy);
+
+                res.status(201);
+                res.send(standardJSONOutput(playlist));
+            } else {
+                unqfy.createPlaylist(req.body.name,req.body.tracks);
+                const playlist = unqfy.getInstanceByAttribute(req.body.name, 'playlist', 'name');
+                saveUNQfy(unqfy);
+
+
+                res.status(201);
+                res.send(standardJSONOutput(playlist));
+            } 
+        } catch{
+            errorHandler(err, req, res);
+        }
+    })
+
+
+router.route('/:id')
+    .get((req, res, err) => { // GET /api/playlists/<id>
+        try {
+            const playlist = unqfy.getInstanceByAttribute(req.params.id, 'artist');
+
+            res.send(standardJSONOutput(artist));
+        } catch {
+            errorHandler(err, req, res);
+        }
+    })
+    .delete((req,res,err) => {// DELETE /api/playlists/<id>
+        try {
+            const playlist = unqfy.getInstanceByAttribute(req.params.id, 'playlist');
+            unqfy.deletePlaylist(playlist);
+            res.status(204);
+            res.send();
+        } catch{
+            errorHandler(err, req, res);
+        }
+    })  
 
 //app.listen(3000);
 
