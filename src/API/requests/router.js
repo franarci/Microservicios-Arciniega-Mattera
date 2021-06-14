2f52f843974532265784e1945f996304caba4aec
LucasMattera <lucasmattera97@gmail.com>
2021-06-14 19:36:39 -0300

    add idea de posible manejo de url invalida

--- src/API/requests/router.js
+++ src/API/requests/router.js
@@ -5,13 +5,25 @@ const {appAlbum} = require('./albumRequests');
 const {appPlaylist} = require('./playlistRequests');
 const { getUNQfy } = require('../../../main');
 
-//hago esto para probar las playlist//
-//voy a llenar este unqfy con tracks para poder crear playlists y lo voy a exportar
-//para que lo usen los request.js
-const unqfy = getUNQfy();
+const {URL_InvalidInexistent_ERROR} =require('../apiErrors')
 
+/* const root2 = express();
+root2.use('/api,',appArtist, appAlbum, appPlaylist);
 
-rootAPI.use('/api',appArtist, appAlbum, appPlaylist);
+const sarasa = express();
+const routeSarasa = express.Router();
+sarasa.use('*', routeSarasa);
+
+routeSarasa.route('/')
+    .get((req,res) => {
+        const err = new URL_InvalidInexistent_ERROR();
+        res.status(err.status);
+        res.json({status: err.status, errorCode: err.errorCode});
+        throw err;
+    })
+
+rootAPI.use('/', root2, sarasa); */
+rootAPI.use('/api', appArtist, appAlbum, appPlaylist);
 rootAPI.listen(3000);
 
 module.exports={unqfy}
