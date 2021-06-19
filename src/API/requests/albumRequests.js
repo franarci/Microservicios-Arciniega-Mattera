59087ddb7e0a80726a21a6d6f0be0bc8cee7630e
franarci <fran.arciniega96@gmail.com>
2021-06-19 01:55:23 -0300

    album requests probadas con errores

--- src/API/requests/albumRequests.js
+++ src/API/requests/albumRequests.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {unqfy, saveUnqfy} = require('./saveAndLoadUNQfy');
-const { errorHandler } = require('../apiErrors');
-
+const { errorHandler } = require('../apiErrors2');
+const {InstanceDoesNotExist} = require('../../errors')
 
 const appAlbum = express();
 const router = express.Router();
@@ -34,22 +34,29 @@ appAlbum.use(errorHandler);
 // donde usar el URL_InvalidInexistent_ERROR?
 
 router.route('/')
-    .post((req, res) => { // POST /api/albums/
-        // ver si el json tiene la forma esperada JSON_Invalid_ERROR
-        // ver si el json tiene valores en todas sus claves JSON_MissingParameter_ERROR
-        const album = unqfy.getInstanceByAttribute(req.body.name, 'album', 'name');
-        const dataAlbum = {name: req.body.name, year:req.body.year};
-        
-        res.status(201);
-        unqfy.addAlbum(req.body.artistId, dataAlbum);
-        saveUnqfy(unqfy);
-        res.send(standardJSONOutput(album));
+    .post((req, res, next) => { // POST /api/albums/
+        const keys = Object.keys(req.body);
+        if(keys.length > 2) {
+            try {
+                const dataAlbum = {name: req.body.name, year:req.body.year};
+                unqfy.addAlbum(req.body.artistId, dataAlbum);
+                saveUnqfy(unqfy);
+                res.status(201);
+                res.send(standardJSONOutput(dataAlbum));
+            } catch (error) {
+                if(error instanceof InstanceDoesNotExist){
+                    next(new Error("RelatedResourceNotFound"));
+                } else throw error;
+            }
+        } else {
+            next(new Error("MissingParameter")); 
+        }
     })
     .get((req, res) => { // GET /api/albums?name=
         const album = unqfy.getInstanceByAttribute(req.query.name, 'album', 'name');
         res.send(standardJSONOutput(album));
     })
-    
+
 router.route('/:id')
     .get((req, res) => { // GET /api/albums/:id
         const album = unqfy.getInstanceByAttribute(req.params.id, 'album');
@@ -62,13 +69,16 @@ router.route('/:id')
         res.status(204);
         res.send();
     })
-    .patch((req, res) => { // PATCH /api/albums/:id
-        // ver si el json tiene la forma esperada JSON_Invalid_ERROR
-        // ver si el json tiene valores en todas sus claves JSON_MissingParameter_ERROR
-        unqfy.modifyInstance(req.params.id, 'album', req.body);
-        saveUnqfy(unqfy);
-        const album = unqfy.getInstanceByAttribute(req.params.id, 'album');
-        res.send(standardJSONOutput(album));
+    .patch((req, res, next) => { // PATCH /api/albums/:id
+        const keys = Object.keys(req.body); 
+        if(keys.length > 0){
+            unqfy.modifyInstance(req.params.id, 'album', req.body);
+            saveUnqfy(unqfy);
+            const album = unqfy.getInstanceByAttribute(req.params.id, 'album');
+            res.send(standardJSONOutput(album));
+        } else {
+            next(new Error("MissingParameter"));
+        }
     })
 
 
