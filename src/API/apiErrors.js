2aad5dc3fc4049fe1404c062f5f40e0c269247ef
franarci <fran.arciniega96@gmail.com>
2021-06-21 17:59:57 -0300

    cambio nombre de apiErrors

--- src/API/apiErrors.js
+++ src/API/apiErrors.js
@@ -1,192 +1,69 @@
+const {InstanceDoesNotExist, InstanceAlreadyExist} = require('../errors')
+
 class APIError extends Error {
-    constructor(name, statusCode, errorCode, message = null) {
-        super(message || name);
-        this.name = name;
+    constructor(statusCode, errorCode, message = null) {
+        super(null);
         this.status = statusCode;
         this.errorCode = errorCode;
+        this.message = message
     }
 }
  
 class AlreadyExists_ERROR extends APIError {
-    constructor(className) {
-        super(`The${className}AlreadyExists`, 409, 'RESOURCE_ALREADY_EXISTS');
-    }  
-}
-
-class AddAlbumToNonExistentArtist_ERROR extends APIError {
     constructor() {
-        super('TheArtistDoesNotExists', 404, 'RELATED_RESOURCE_NOT_FOUND');
+        super(409, 'RESOURCE_ALREADY_EXISTS', "Resource already exists");
     }  
 }
 
-class URL_InvalidInexistent_ERROR extends APIError {
+class NonExistentRelated_ERROR extends APIError {
     constructor() {
-        super('InvalidOrInexistentURL', 404, 'RESOURCE_NOT_FOUND');
-    }  
+        super(404, 'RELATED_RESOURCE_NOT_FOUND', "Related resource does not exist");
+    }
 }
 
-class ALBUMARTIST_DelGetPostInexistent_ERROR extends APIError {
-    constructor(className) {
-        super(`InvalidOrInexistent${className}`, 404, 'RESOURCE_NOT_FOUND', 'hola pepe');
-    }  
-}
-
-class JSON_Invalid_ERROR extends APIError {
+class NonExistent_ERROR extends APIError {
     constructor() {
-        super('InvalidJSONError', 400, 'BAD_REQUEST');
-    }  
+        super(404, 'RESOURCE_NOT_FOUND', "Resource does not exist");
+    }
 }
 
-class JSON_MissingParameter_ERROR extends APIError {
+
+class Invalid_ERROR extends APIError {
     constructor() {
-        super('MissingParameterError', 400, 'BAD_REQUEST');
+        super(400, 'BAD_REQUEST', "Invalid JSON, invalid input key or missing parameter");
     }  
 }
 
 class UNEXPECTED_Failure_ERROR extends APIError {
     constructor() {
-        super('UnexpectedFailure', 500, 'INTERNAL_SERVER_ERROR');
+        super(500, 'INTERNAL_SERVER_ERROR');
     }  
 }
 
-class LYRICS_DoesntMatch_ERROR extends APIError {
-
-    constructor() {
-        super('TheSongWithThisLyricsDoesNotExists', 404, 'RESOURCE_NOT_FOUND');
-    }  
-}
-
-class PLAYLIST_AddInexistentTrack_ERROR extends APIError {
-    constructor() {
-        super('TheSongWithThisLyricsDoesNotExists', 404, 'RESOURCE_NOT_FOUND');
-    }  
-}
-
-class PLAYLIST_DelGetInexistent_ERROR extends APIError {
-    constructor() {
-        super('TheSongWithThisLyricsDoesNotExists', 404, 'RESOURCE_NOT_FOUND');
-    }  
-}
-
-class PLAYLIST_MissingParameter_ERROR extends APIError {
-    constructor() {
-        super('TheSongWithThisLyricsDoesNotExists', 404, 'RESOURCE_NOT_FOUND');
-    }  
-}
-
-function errorHandler(error, request, res, next){ // <------ ESTE NEXT ES MUCHO MUY IMPORTANTE
-    const reqQueryName = request.query.name;
-    const reqBody = request.body;
-    const reqParamsId = request.params.id;
+function errorHandler(error, request, res, next){
+    console.error(error);
     const errMsg = error.message;
+    var err;
 
-    const artistName_DoesNot_ExistsMessage = `The artist with name ${reqQueryName} does not exist`;
-    const artistName_Already_ExistsMessage = `The artist with name ${reqQueryName} already exist`;
-    const artistID_Body_DoesNot_ExistsMessage = `The artist with id ${reqBody.artistId} does not exist`;
-    const artistID_Params_DoesNot_ExistsMessage = `The artist with id ${reqParamsId} does not exist`;
 
-    const albumName_DoesNot_ExistsMessage = `The album with name ${reqQueryName} does not exist`;
-    const albumName_Already_ExistsMessage = `The album with name ${reqQueryName} already exist`;
-    const albumID_DoesNot_ExistsMessage = `The album with id ${reqParamsId} does not exist`;
-
-    if(!request.query){
-        if (errMsg == artistName_Already_ExistsMessage){ 
-            const err = new AlreadyExists_ERROR('Artist');
-            res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        } else if (errMsg == albumName_Already_ExistsMessage){
-            const err = new AlreadyExists_ERROR('Album');
-            res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        } else if (errMsg == artistName_DoesNot_ExistsMessage){
-            const err = new ALBUMARTIST_DelGetPostInexistent_ERROR('Album');
-            res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        }
-    }
-    else if(!!reqBody && !reqBody){
-        JSONerrorHandler(request);
-        if (errMsg == artistID_Body_DoesNot_ExistsMessage){
-            const err = new AddAlbumToNonExistentArtist_ERROR('Artist');
-            res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        }
-    }
-    else if(!!request.params){
-        if (errMsg == artistID_Params_DoesNot_ExistsMessage){
-            const err = new ALBUMARTIST_DelGetPostInexistent_ERROR('Album');
-            res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        } else if (errMsg == albumID_DoesNot_ExistsMessage){
-            const err = new ALBUMARTIST_DelGetPostInexistent_ERROR('Album');
-            res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        } else if (errMsg == `The playilist with id ${reqParamsId} does not exist`){
-            const err = new PLAYLIST_DelGetInexistent_ERROR();
-            res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        }
-    }
-    else if(!!request.query){
-        if (errMsg == albumName_DoesNot_ExistsMessage){
-            const err = new ALBUMARTIST_DelGetPostInexistent_ERROR('Album');
-            res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        }    
-    }
-
-    else if (errMsg == ''){
-        const err = new LYRICS_DoesntMatch_ERROR();
+    if(error.type === 'entity.parse.failed' || errMsg === 'InvalidInputKey' || errMsg === "MissingParameter"){
+        err = new Invalid_ERROR();
         res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-    } 
-    /* else if (errMsg == `The track with name ${} does not exist`){ //como traigo el nombre del track que no existe
-        const err = new PLAYLIST_AddInexistentTrack_ERROR();
+    }
+    else if(error instanceof InstanceDoesNotExist || errMsg === 'Invalid route') {
+        err = new NonExistent_ERROR();
         res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-    } */
-    else if (errMsg == ''){
-        const err = new PLAYLIST_MissingParameter_ERROR();
+    }
+    else if (errMsg === 'RelatedResourceNotFound'){
+        err = new NonExistentRelated_ERROR;
         res.status(err.status).json({status: err.status, errorCode: err.errorCode});
+    }
+    else if (error instanceof InstanceAlreadyExist) {
+        err = new AlreadyExists_ERROR;
+        res.status(err.status).json({status: err.status, errorCode: err.errorCode})
     } else {
-        const err = new UNEXPECTED_Failure_ERROR();
-        res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-    } 
-}
-
-const keysOfBody_POST_PATCH_apiArtists = ['name', 'country'];
-
-const keysOfBody_POST_apiAlbums = ['artistId', 'name', 'year'];
-const keysOfBody_PATCH_apiAlbums = ['year'];
-
-const keysOfBody_POST_apiPlaylists_genres = ['name', 'maxDuration', 'genres'];
-const keysOfBody_POST_apiPlaylists_tracks = ['name', 'tracks'];
-
-function jsonShapeChecker(jsonKeys, referenceKeys){
-    if(jsonKeys != referenceKeys){
-        const err = new JSON_Invalid_ERROR();
-        res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-    } else{
-        const err = new JSON_MissingParameter_ERROR();
-        res.status(err.status).json({status: err.status, errorCode: err.errorCode});
+        err = new UNEXPECTED_Failure_ERROR;
+        res.status(err.status).json({status: err.status, errorCode: err.errorCode})
     }
 }
-
-function JSONerrorHandler(req){
-    const data = req;
-    const bodyJSONKeys = data.body.keys();
-
-    const requestShape = `${data.method} ${data.originalUrl}`;
-    if(requestShape == 'POST /api/artists' || requestShape == `PATCH /api/artists/${data.params.id}`){
-        jsonShapeChecker(bodyJSONKeys, keysOfBody_POST_PATCH_apiArtists);
-    } else if (requestShape == 'POST /api/albums'){
-        jsonShapeChecker(bodyJSONKeys, keysOfBody_POST_apiAlbums);
-    } else if (requestShape == `PATCH /api/albums/${req.params.id}`){
-        jsonShapeChecker(bodyJSONKeys, keysOfBody_PATCH_apiAlbums);
-    } else if (bodyJSONKeys == keysOfBody_POST_apiPlaylists_genres){
-        jsonShapeChecker(bodyJSONKeys, keysOfBody_POST_apiPlaylists_genres);
-    } else if (bodyJSONKeys == keysOfBody_POST_apiPlaylists_tracks){
-        jsonShapeChecker(bodyJSONKeys, keysOfBody_POST_apiPlaylists_tracks);
-    }
-} 
-
-function verifyURL(recivedURL, desiredURL, res){
-    if(recivedURL != desiredURL){
-        const err = new URL_InvalidInexistent_ERROR();
-        res.status(err.status).json({status: err.status, errorCode: err.errorCode});
-        throw err;
-    }
-}
-
-
 module.exports = {errorHandler}
