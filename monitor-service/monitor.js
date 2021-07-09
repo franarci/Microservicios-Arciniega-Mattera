c914a5a7eca1112caacbeb9751bdb0db2d8d2e32
LucasMattera <lucasmattera97@gmail.com>
2021-07-09 12:44:10 -0300

    mode rename monitor api module

--- monitor-service/monitor.js
+++
@@ -1,3 +0,0 @@
-class Monitor{}
-
-module.exports = {Monitor:Monitor}
---
+++ monitor-service/src/API/apiMonitor.js
@@ -0,0 +1,83 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let appMonitor = express();
+let router = express.Router();
+
+let checkStatusUNQfy      = require('./statusUNQfy.js');
+//let checkStatusNewsletter = require('./statusNewsletter');
+//let checkStatusLogging    = require('./statusLogging');
+
+let discordNotify = require('./discordNotify');
+
+//let stopMonitor = require('./statusUNQfy.js');
+
+let errors = require('./apiErrors');
+let serviceStatus = false;
+
+//appMonitor.use(bodyParser.json());
+appMonitor.use('/api/monitor',router);
+
+let unqfyMonitor;
+let newsletterMonitor;
+let loggingMonitor;
+
+function checkServices(){
+    if(serviceStatus){
+        unqfyMonitor = checkStatusUNQfy();
+        //newsletterMonitor = checkStatusNewsletter();
+        //loggingMonitor = checkStatusLogging();
+    }
+}
+
+function stopMonitors(){
+    unqfyMonitor.stop();
+    delete unqfyMonitor;
+
+    newsletterMonitor.stop();
+    delete newsletterMonitor;
+
+    loggingMonitor.stop();
+    delete loggingMonitor;
+
+}    
+
+router.route('/statusUNQfy')
+    .post((req,res) =>{
+        if(serviceStatus){
+            //console.log(req.body)
+            discordNotify('El servicio UNQfy ha dejado de funcionar');
+            //res.json(status.StatusUNQfy=req.body.StatusUNQfy);
+        }
+});
+
+router.route('/services')
+    .get((req, res) => {
+
+    })
+
+router.route('/notify')
+    .post((req, res) => {
+
+    })
+
+router.route('/start')
+    .post((req, res) => {
+        serviceStatus = true;
+        checkServices();
+        console.log("MONITOR SERVICE ON");
+        res.status(200).json({status: 200, message: "El monitor esta activo"});
+    })
+
+router.route('/stop')
+    .post((req, res) => {
+        serviceStatus = false;
+        stopMonitors();
+        console.log("MONITOR SERVICE OFF");
+        res.status(200).json({status: 200, message: "El monitor esta desactivado"});
+    })
+
+appMonitor.listen(5002, () =>{ console.log('Monitor listening on port 5002') });
+
+
+module.exports = checkServices;
