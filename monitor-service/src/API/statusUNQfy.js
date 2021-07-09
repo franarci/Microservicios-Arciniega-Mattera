d04a2c4e33400cbd7e3227f346c1e22d82560c30
LucasMattera <lucasmattera97@gmail.com>
2021-07-09 12:44:56 -0300

    add unqfystatus module

--- monitor-service/src/API/statusUNQfy.js
+++ monitor-service/src/API/statusUNQfy.js
@@ -1,53 +1,38 @@
 const fetch = require('node-fetch');
-const Monitor = require('ping-monitor')
+const Monitor = require('ping-monitor');
 
 function checkStatusUNQfy(){
     
     const myMonitor = new Monitor({
-        website:'http://localhost:8000',
+        website:'http://localhost:5001',
         title:'UNQFy',
-        interval: 1
+        interval: 10,
+        config: { intervalUnits: 'seconds' },
     });
     
     myMonitor.on('down',function(res,state){
-        if(myMonitor.totalRequestPut > 1){
-            console.log('UNQfy funcionando con normalidad')
-            console.log('put' + myMonitor.totalRequestPut)
-        }else{
-            myMonitor.totalRequestPost = myMonitor.resetValPost
-            return fetch('http://localhost:8003/api/statusUNQfy', {
-                method:'PUT',
-                headers:{
-                    'Content-Type': 'application/json'
-                },
-                body:JSON.stringify({StatusUNQfy: 'ON'})
-            }).then(res => res.json())
+        myMonitor.totalRequestPost = myMonitor.resetValPost
+        return fetch('http://localhost:5002/api/statusUNQfy', {
+            method:'POST',
+            headers:{ 'Content-Type': 'application/json' },
+            body:JSON.stringify({unqfyStatus: 'OFF'})
+        })
+            .then(res => res.json())
             .catch(err => console.error(err))
-        }    
     });   
 
-
     myMonitor.on('error', function(res){
-        if(myMonitor.totalRequestPost > 1){
-            console.log('UNQfy notificando a slack')
-            console.log('post' + myMonitor.totalRequestPost)
-        }else{
-            myMonitor.totalRequestPut = myMonitor.resetValPut
-            return fetch('http://localhost:8003/api/statusUNQfy', {
-                method: 'POST', 
-                headers:{
-                    'Content-Type': 'application/json'
-                },
-                body:JSON.stringify({StatusUNQfy: 'OFF'})
-            }).then(res => res.json())
+        myMonitor.totalRequestPut = myMonitor.resetValPut
+        return fetch('http://localhost:5002/api/statusUNQfy', {
+            method: 'POST', 
+            headers:{ 'Content-Type': 'application/json' },
+            body:JSON.stringify({unqfyStatus: 'OFF'})
+        })
+            .then(res => res.json())
             .catch(err => console.error(err))
-        }
     });
-}   
-function stopMonitor(){
-    this.myMonitor.stop()
-    delete this.myMonitor
-    
-    }    
 
-module.exports = checkStatusUNQfy, stopMonitor;
+    return myMonitor;
+}   
+
+module.exports = checkStatusUNQfy;
