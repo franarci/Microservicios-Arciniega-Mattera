fb8deb460639e6a48efbfd899c7f682fb0b8e6a5
LucasMattera <lucasmattera97@gmail.com>
2021-07-13 20:48:31 -0300

    export discordNotify via rp

--- monitor-service/src/API/discordNotify.js
+++ monitor-service/src/API/discordNotify.js
@@ -7,31 +7,35 @@ const wbhook_id = process.env.WEBHOOK_ID;
 const wbhook_tkn = process.env.WEBHOOK_TKN;
 
 const webhookClient = new WebhookClient(
-    process.env.WEBHOOK_ID,
-    process.env.WEBHOOK_TKN,
+    wbhook_id,
+    wbhook_tkn,
 );
 
 let date = new Date();
 
 function discordNotify_fetch(msg){
+    const message = JSON.stringify(date + ': ' + msg);
     return fetch(`https://discord.com/api/webhooks/${wbhook_id}/${wbhook_tkn}`,{
         method: 'POST',
-        body: JSON.stringify({text:date + ': ' + msg}),
-        headers: {
-            'Content-type' : 'application/json'
-        }
-    }).then(response => response.json()).then(console.log)
-    .catch(console.log)
+        headers: {'Content-type' : 'application/json'},
+        body: {'content': 'test disc notify fetch'},
+    })
+        .then(response => response.json()).then(console.log)
+        .catch(console.log)
 }
 
 function discordNotify_rp(msg){
+    const message = JSON.stringify(date + ': ' + msg);
     const options = {
         uri: `https://discord.com/api/webhooks/${wbhook_id}/${wbhook_tkn}`,
         headers: {'Content-Type': ' application/json',},
-        body: `${msg}`,
+        body: {'content': date + ': ' + msg},
         json:true
     }
-    const res = rp.get(options);
+    const res = 
+        rp.post(options)
+            .then(response => response.json()).then(console.log)
+            .catch(console.log)
     console.log(res);
 }
 
@@ -39,14 +43,14 @@ function main(){
     if(process.argv[2] == 'fetch'){discordNotify_fetch('test msg discord notify fetch')} 
     else if (process.argv[2] == 'rp'){discordNotify_rp('test msg discord notify rp')}
     else if (process.argv[2] == 'wbhk`'){
-        webhookClient.send(`${date} test msg discord notify web hook client`)
+        webhookClient.send( `test msg discord notify web hook client`)
             .then(message => console.log(`mensaje enviado: ${message.content}`))
             .catch(console.error)
     }
 }
 
 
-console.log(process.env.WEBHOOK_ID);
-console.log(process.env.WEBHOOK_TKN);
-main();
-//module.exports = discordNotify;
+//console.log(process.env.WEBHOOK_ID);
+//console.log(process.env.WEBHOOK_TKN);
+//main();
+module.exports = discordNotify_rp;
