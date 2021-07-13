e00590919f3fee137be9be75698a3b04c528dd7f
franarci <fran.arciniega96@gmail.com>
2021-07-12 22:35:30 -0300

    modelo LoggingObserver

--- logging-service/src/LoggingObserver.js
+++ logging-service/src/LoggingObserver.js
@@ -1,5 +1,27 @@
-class LoggingObserver {
-    update(event, object){
-        
+let fs = require('fs');
+
+const log = {
+    newAlbum : {msg : "new album:"},
+    newTrack: {msg : "new track:"},
+    newArtist: {msg : "new artist:"},
+    removedArtist: {msg : "removed artist:"},
+    removedAlbum: {msg : "removed album:"},
+    removedTrack: {msg : "removed track:"}
+}
+
+class Observer {
+    update();
+}
+
+class LoggingObserver extends Observer{
+     update(event, object){
+        return fetch(`http://localhost:${puerto}/api/logging`,{
+            method : 'post',
+            body: JSON.stringify({mensaje : mensaje, tipo: tipo}),
+            headers: {
+                'Content-Type' : 'application/json'
+            } 
+        }).then(res => res.json())    
+
     }
 }
