a27ada2a7ee76045ecfdfc4b140bf1539aa54878
franarci <fran.arciniega96@gmail.com>
2021-07-17 18:54:03 -0300

    cambios que ya habia hecho antes. No se por que me aparecen de nuevo

--- logging-service/src/API/loggingApi.js
+++ logging-service/src/API/loggingApi.js
@@ -3,9 +3,9 @@ let express = require('express');
 let token = process.env.LOGGING_TKN;
 let running = true;
 var winston  = require('winston');
-var {Loggly} = require('winston-loggly-bulk');
-winston.add(new Loggly({
-    token: token, ///////CAMBIAR POR VARIABLE
+var winlog = require('winston-loggly-bulk');
+winston.add(new winlog.Loggly({
+    token: "80214558-74a9-4715-abc3-cc31c0bbc2ed", ///////CAMBIAR POR VARIABLE
     subdomain: "franarci",
     tags: ["Winston-NodeJS"],
     json: true
@@ -14,12 +14,13 @@ winston.add(new Loggly({
 var log4js = require("log4js");
 var logger = log4js.getLogger();
 log4js.configure({
-    appenders: { cheese: { type: "file", filename: "logs.log" } },
+    appenders: { cheese: { type: "file", filename: "logs.txt" } },
     categories: { default: { appenders: ["cheese"], level: "error" } }
 });
 
 let appLogging = express();
 let router = express.Router();
+router.use(express.json());
 
 let {errorHandler} = require('./apiErrors');
 appLogging.use(errorHandler);
@@ -27,11 +28,12 @@ appLogging.use(errorHandler);
 appLogging.use('/api',router);
 
 router.route('/logging')
-    .post((req, res)=>{ // POST /api/logging/
+    .post((req, res,next)=>{ // POST /api/logging/
         if(running){
             try{
                 const level = req.body.level;
                 const msg = req.body.message;
+                logger.level = level;
                 switch(level){
                     case "error":
                         logger.error(msg);
@@ -46,17 +48,23 @@ router.route('/logging')
                         logger.info(msg);
                         break;
                 }
+                winston.log(req.body.level, req.body.message);
+
             } catch(e) {
                 next(e);
             }
         }
         res.status(200);
+        res.json("ok");
     })
 
 router.route('/start')
     .post((req, res) => {
         running=true;
         console.log("LOGGING SERVICE ON");
+        logger.level = "info";
+        logger.info('Server running');
+        winston.log("info", "Server running");
         res.status(200).json({status: 200, message: "El servicio de logging esta activo"});
     })
 
@@ -64,11 +72,19 @@ router.route('/stop')
     .post((req, res) => {
         running = false;
         console.log("LOGGING SERVICE OFF");
+        logger.level = 'warning';
+        logger.warn("Logging service stopped");
+        winston.log('warn', 'Logging service stopped')
         res.status(200).json({status: 200, message: "El servicio de logging esta desactivado"});
     })
 
 router.route('/status').get((req, res) => { res.status(200).send(JSON.stringify('OK'))});
 
-appLogging.listen(5003, () =>{ console.log('Logging listening on port 5003') });
+appLogging.listen(5003, () =>{ 
+    console.log('Logging listening on port 5003');
+    logger.level = "info";
+    logger.info('Server running');
+    winston.log("info", "Server running");
+ });
 
 
