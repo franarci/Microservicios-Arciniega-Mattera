1f7687ad4759d2eef2e9a4d696850dd6e8caa8b6
franarci <fran.arciniega96@gmail.com>
2021-07-16 21:18:38 -0300

    update de LoggingObserver

--- UNQfy-Service/src/observer/loggingObserver.js
+++ UNQfy-Service/src/observer/loggingObserver.js
@@ -1,16 +1,37 @@
 let fs = require('fs');
-const Observer = require('./observer')
+const Observer= require('./observer');
+const LoggingClient = require('../clientes/Logging/LoggingClient');
 const { newLog } = require('../../../logging-service/src/API/loggingApi') ;
 
+const logs = {
+   newAlbum : "New album: ",
+   newTrack:  "New track: ",
+   newArtist:  "New artist: ",
+   removedArtist:  "Removed artist: ",
+   removedAlbum: "Removed album: ",
+   removedTrack: "Removed track: ",
+   error:  "An error occurred: " ,
+   warning: "Warning: ",
+   debug: "Debug: "
+};
 
 class LoggingObserver extends Observer{
-    async update(event, object){
-         let log= {
-            msg: logs[event] + object.name,
-            nivel: ""
-         }
-       await newLog(log);
-    }
+
+   update(event, eventObject){
+      switch(event){
+         case "error":
+            LoggingClient.logError(logs[error]+eventObject.msg);
+            break;
+         case "warning":
+            LoggingClient.logWarning(logs[warning]+eventObject.msg);
+            break;
+         case "debug":
+            LoggingClient.logDebug(logs[event]+eventObject.msg);
+         default:
+            LoggingClient.logInfo(logs[event]+eventObject.name);
+      }
+
+   }
 }
 
 module.exports = LoggingObserver;
