c338f5eb038d2371524673d81bb5f49ab6fab1ca
franarci <fran.arciniega96@gmail.com>
2021-07-17 00:37:40 -0300

    Instancio correctamente LoggingClient

--- UNQfy-Service/src/observer/loggingObserver.js
+++ UNQfy-Service/src/observer/loggingObserver.js
@@ -1,18 +1,16 @@
 let fs = require('fs');
 const Observer= require('./observer');
 const LoggingClient = require('../clients/Logging/LoggingClient');
+const LoggingClientInstance = new LoggingClient.LogginClient();
 const { newLog } = require('../../../logging-service/src/API/loggingApi') ;
 
-const logs = {
+const events = {
    newAlbum : "New album: ",
    newTrack:  "New track: ",
    newArtist:  "New artist: ",
    removedArtist:  "Removed artist: ",
    removedAlbum: "Removed album: ",
    removedTrack: "Removed track: ",
-   error:  "An error occurred: " ,
-   warning: "Warning: ",
-   debug: "Debug: "
 };
 
 class LoggingObserver extends Observer{
@@ -20,15 +18,15 @@ class LoggingObserver extends Observer{
    update(event, eventObject){
       switch(event){
          case "error":
-            LoggingClient.logError(logs[error]+eventObject.msg);
+            LoggingClientInstance.logError(eventObject.msg);
             break;
          case "warning":
-            LoggingClient.logWarning(logs[warning]+eventObject.msg);
+            LoggingClientInstance.logWarning(eventObject.msg);
             break;
          case "debug":
-            LoggingClient.logDebug(logs[event]+eventObject.msg);
+            LoggingClientInstance.logDebug(eventObject.msg);
          default:
-            LoggingClient.logInfo(logs[event]+eventObject.name);
+            LoggingClientInstance.logInfo(events[event]+eventObject.name);
       }
 
    }
