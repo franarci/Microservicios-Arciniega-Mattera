7a1cbf4dc8e2b1a49648dedf1937680db7e9fcbe
franarci <fran.arciniega96@gmail.com>
2021-07-17 23:01:36 -0300

    newsletterObserver

--- UNQfy-Service/src/observer/newsletterObserver.js
+++ UNQfy-Service/src/observer/newsletterObserver.js
@@ -4,21 +4,9 @@ const NLClientInstance = new NewsletterClient.NewsletterClient();
 class NewsletterObserver extends Observer{
 
     update(event, eventData){
-       switch(event){
-          case "error":
-             LoggingClientInstance.logError(eventData.msg);
-             break;
-          case "warning":
-             LoggingClientInstance.logWarning(eventData.msg);
-             break;
-          case "debug":
-             LoggingClientInstance.logDebug(eventData.msg);
-             break;
-          default:
-             LoggingClientInstance.logInfo(events[event]+eventData.changedObject.name);
-             break;
-       }
- 
+        if(event == "newAlbum"){
+            NLClientInstance.newAlbum(eventData);
+        }
     }
  }
  
